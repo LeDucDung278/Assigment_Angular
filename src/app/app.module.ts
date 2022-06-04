d97a951e0dbf4ae33786cda4fbeaf411c10b0517
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-06-04 16:38:24 +0700

    create client-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientLayoutComponent
   ],
   imports: [
     BrowserModule,
